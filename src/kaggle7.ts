29daf737dfec728dd50125fcc5af1cf59b63fb4f
Emre Guneyler <eguneys@gmail.com>
2025-01-27 16:58:21 +0300

    id

--- src/kaggle7.ts
+++ src/kaggle7.ts
@@ -39,9 +39,9 @@ type HMoves = {
     moves: Move[]
 }
 
-type Context = Record<string, Square>
+type Context = Record<string, Square>[]
 
-function h_moves_recurse(node: Line, h: Hopefox, cx: Context[]) {
+function h_moves_recurse(node: Line, h: Hopefox, cx: Context) {
 
 
     let moves: Move[] = []
@@ -57,12 +57,31 @@ function h_moves_recurse(node: Line, h: Hopefox, cx: Context[]) {
 
 }
 
+/*
+
+id_08OfC
+
+r =P
+K =P
+Q =B
+b =B +K
+ *b =R +K
+ Q =b
+  r =P
+   *r =h1 #
+   K =r
+    n =R +K +Q
+   Q =n
+    n' =Q
+     K =r
+      n' =R
+*/
 export function find_san7(fen: string, rules: string) {
 
     let h = Hopefox.from_fen(fen)
     let root = parse_rules(rules)
 
-    let cx = [{}]
+    let cx: Context = []
 
     for (let child of root.children) {
         h_moves_recurse(child, h, cx)
