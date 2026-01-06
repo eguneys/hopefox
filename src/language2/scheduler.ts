1a32317117e2862b4008ef42580da0a2ff49038e
eguneys <eguneys@gmail.com>
2026-01-06 22:26:30 +0300

    shceduler fix

--- src/language2/scheduler.ts
+++ src/language2/scheduler.ts
@@ -50,7 +50,6 @@ class Scheduler {
 
 
     suspend_prefix_to_request_facts(prefix: Prefix, fact_key: FactKey) {
-        // how do we get fact key from a prefix ??
         let list = this.pending_prefixes.get(fact_key)
         if (!list) {
             this.pending_prefixes.set(fact_key, new Set([prefix]))
@@ -108,6 +107,47 @@ class Scheduler {
 }
 
 type RowId = number
+
+class RelationManager {
+    base: Relation
+    name: string
+    index_start_world: Map<WorldId, RowId[]> = new Map()
+
+    add_rows(world_id: WorldId, rows: Row[]) {
+        for (const row of rows) {
+            const row_id = this.add_rows.length
+            this.base.rows.push(row)
+
+            if (!this.index_start_world.has(world_id)) {
+                this.index_start_world.set(world_id, [])
+            }
+            this.index_start_world.get(world_id)!.push(row_id)
+        }
+    }
+
+    add_row(row: Row) {
+        let row_id = this.base.rows.length
+        this.base.rows.push(row)
+
+        const w = row.get('start_world_id')!
+        if (w !== undefined) {
+            let list = this.index_start_world.get(w)
+            if (!list) {
+                this.index_start_world.set(w, [row_id])
+            } else {
+                list.push(row_id)
+            }
+        }
+    }
+
+    get_row_ids_starting_at_world_id(world_id: WorldId): RowId[] {
+        return this.index_start_world.get(world_id) ?? []
+    }
+    get_row(row_id: RowId) {
+        return this.base.rows[row_id]
+    }
+}
+
 // M is a move-fact relation
 // line M c1 c2 c3
 // A prefix is a partially matched line.
@@ -117,12 +157,12 @@ type Prefix = {
 }
 
 
-function prefix_last_world_id(prefix: Prefix, initial_world: WorldId): WorldId {
+function prefix_required_last_world_id(M: RelationManager, prefix: Prefix, initial_world: WorldId): WorldId {
     if (prefix.length === 0) {
         return initial_world
     }
-    const last_row = get_row(prefix.bindings[prefix.length - 1])
-    return last_row.end_world_id
+    const last_row = M.get_row(prefix.bindings[prefix.length - 1])
+    return last_row.get('end_world_id')!
 }
 
 function extend_prefix(prefix: Prefix, row_id: RowId): Prefix {
@@ -132,6 +172,7 @@ function extend_prefix(prefix: Prefix, row_id: RowId): Prefix {
     }
 }
 
+
 class IdeaJoin {
 
     scheduler: Scheduler
@@ -140,7 +181,7 @@ class IdeaJoin {
 
     fact: Fact
 
-    M: Column
+    M: RelationManager
 
     initial_world_id: WorldId
 
@@ -148,21 +189,25 @@ class IdeaJoin {
         this.fact = fact
     }
 
+    get_row(row_id: RowId) {
+        return this.M.get_row(row_id)
+    }
+
     insert_line_into_relation(prefix: Prefix) {
 
         const row = new Map()
 
-        const first = get_row(prefix.bindings[0])
-        const last = get_row(prefix.bindings[prefix.length - 1])
+        const first = this.get_row(prefix.bindings[0])
+        const last = this.get_row(prefix.bindings[prefix.length - 1])
 
-        row.set('start_world_id', first.start_world_id)
-        row.set('end_world_id', last.end_world_id)
+        row.set('start_world_id', first.get('start_world_id'))
+        row.set('end_world_id', last.get('end_world_id'))
 
 
         for (let i = 0; i < prefix.bindings.length; i++) {
-            const r = get_row(prefix.bindings[i])
-            row.set(`from${i+1}`, r.from)
-            row.set(`to${i+1}`, r.to)
+            const r = this.get_row(prefix.bindings[i])
+            row.set(`from${i+1}`, r.get('from'))
+            row.set(`to${i+1}`, r.get('to'))
         }
 
         // output.emit(row)
@@ -178,8 +223,7 @@ class IdeaJoin {
                 continue
             }
 
-            // ??
-            let next_world_id = prefix_last_world_id(prefix, this.initial_world_id)
+            let next_world_id = prefix_required_last_world_id(this.M, prefix, this.initial_world_id)
 
             let needs_fact_key = hash_fact_key(this.M.name, next_world_id)
             if (!this.scheduler.is_facts_joined(needs_fact_key)) {
@@ -187,14 +231,19 @@ class IdeaJoin {
                 continue
             }
 
-
-            for (let row of this.M.get_rows_starting_at(next_world_id)) {
-                let new_prefix = extend_prefix(prefix, row)
+            let [start_row_id, end_row_id] = this.M.get_row_ids_starting_at_world_id(next_world_id)
+            for (let row_id = start_row_id; row_id < end_row_id; row_id++) {
+                let new_prefix = extend_prefix(prefix, row_id)
 
                 if (this.constraints_hold(new_prefix)) {
                     this.worklist.push(new_prefix)
                 }
             }
         }
+    }
+
+
+    constraints_hold(prefix: Prefix) {
+        return false
     }
 }
