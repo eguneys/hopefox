9662c9c0560b049a9748ff6f7f7507d879065eba
eguneys <eguneys@gmail.com>
2026-01-07 17:07:37 +0300

    debugging

--- test/world2.test.ts
+++ test/world2.test.ts
@@ -60,11 +60,23 @@ idea double_captures
 idea check_to_lure_into_double_capture
   line blockable_checks double_captures
   blockable_checks.block_to = double_captures.to
+
+
+idea check_to_lure_into_hanging_capture
+  line blockable_checks captures_moves
+  blockable_checks.block_to = captures_moves.to
+
 `.trim()
 
 let patterns = [
-    'check_to_lure_into_double_capture'
+    'check_to_lure_into_double_capture',
+    'check_to_lure_into_hanging_capture',
+    'double_captures',
+    'checks_moves',
   ]
+
+  console.log(solve_n(5, rules, 'double_captures'))
+  return
 
   for (let i = 0; i < 100; i++) {
     let res = minmax_solve_loose(i, rules, patterns)
@@ -72,98 +84,6 @@ let patterns = [
       break
     }
   }
-})
-
-
-it('more ideas', () => {
-
-    let rules = `
-fact pressures
-     .from = attacks.from
-     .to = attacks.to
-  attacks.to = occupies.square
-
-fact checks0
-     .from = attacks2.from
-     .to = attacks2.to
-     .check = attacks2.to2
-     .piece = occupies.piece
-  attacks2.to2 = occupies.square
-  attacks2.to2 != attacks2.from
-
-fact checks
-     .from = checks0.from
-     .to = checks0.to
-     .check = checks0.check
-  checks0.piece = KING
-
-fact captures
-     .from = attacks.from
-     .to = attacks.to
-  attacks.to = occupies.square
-
-fact blocks
-     .from = attacks.from
-     .to = attacks.to
-     .block_from = pressures.from
-     .block_to = pressures.to
-  attacks.to between pressures.from pressures.to
-
-legal checks_moves
-legal captures_moves
-legal blocks_moves
-
-idea blockable_checks
-   line checks_moves blocks_moves
-     .check_from = checks_moves.from
-     .check_to = checks_moves.to
-     .block_from = blocks_moves.from
-     .block_to = blocks_moves.to
-  blocks_moves.block_from = checks_moves.to
-  blocks_moves.block_to = checks_moves.check
-
-idea double_captures
-  alias c2 captures_moves
-  alias c3 captures_moves
-  line captures_moves c2 c3
-  exchange_square = captures_moves.to
-  exchange_square = c2.to
-  exchange_square = c3.to
-
-idea check_to_lure_into_double_capture
-  line blockable_checks double_captures
-  blockable_checks.block_to = double_captures.to
-`.trim()
-
-;`
-
-idea double_capture
-  alias c2 captures_moves
-  alias c3 captures_moves
-  line captures_moves c2 c3
-    .from = captures_moves.from
-    .to = captures_moves.to
-  c2.to = captures_moves.to
-    .from2 = c2.from
-  c3.to = _.to
-
-idea check_to_lure_into_double_capture
-  line blockable_checks double_captures
-  blockable_checks.block_to = double_captures.to
-`
-
-
-  //bench(rules, 'check_to_lure_into_double_capture')
-
-  solve_n(1, rules, 'checks_moves')
-  return
-
-  let link = puzzles[0].link
-  console.log(link)
-  let fen = puzzles[0].move_fens[0]
-
-  //fen = '8/3Qnk1p/8/4B2b/Pp2p3/1P2P3/5PPP/2rR2K1 b - - 6 33'
-  solve_n(0, rules, 'checks_moves')
 })
 
 
@@ -186,7 +106,11 @@ function minmax_solve_loose(n: number, rules: string, columns: string[]) {
         console.log(n)
         console.log(link)
         console.log(puzzles[n].sans, '\nexpected but found\n', sans.slice(0, 3))
+        console.log(column)
         result = false
+      } else {
+        result = true
+        break
       }
     }
   }
@@ -257,3 +181,79 @@ const find_solving_sans = (a: SAN[][], b: SAN[]) => {
 
     return true
 }
+
+
+
+it.skip('more ideas', () => {
+
+    let rules = `
+fact pressures
+     .from = attacks.from
+     .to = attacks.to
+  attacks.to = occupies.square
+
+fact checks0
+     .from = attacks2.from
+     .to = attacks2.to
+     .check = attacks2.to2
+     .piece = occupies.piece
+  attacks2.to2 = occupies.square
+  attacks2.to2 != attacks2.from
+
+fact checks
+     .from = checks0.from
+     .to = checks0.to
+     .check = checks0.check
+  checks0.piece = KING
+
+fact captures
+     .from = attacks.from
+     .to = attacks.to
+  attacks.to = occupies.square
+
+fact blocks
+     .from = attacks.from
+     .to = attacks.to
+     .block_from = pressures.from
+     .block_to = pressures.to
+  attacks.to between pressures.from pressures.to
+
+legal checks_moves
+legal captures_moves
+legal blocks_moves
+
+idea blockable_checks
+   line checks_moves blocks_moves
+     .check_from = checks_moves.from
+     .check_to = checks_moves.to
+     .block_from = blocks_moves.from
+     .block_to = blocks_moves.to
+  blocks_moves.block_from = checks_moves.to
+  blocks_moves.block_to = checks_moves.check
+
+idea double_captures
+  alias c2 captures_moves
+  alias c3 captures_moves
+  line captures_moves c2 c3
+  exchange_square = captures_moves.to
+  exchange_square = c2.to
+  exchange_square = c3.to
+
+idea check_to_lure_into_double_capture
+  line blockable_checks double_captures
+  blockable_checks.block_to = double_captures.to
+`.trim()
+
+  //bench(rules, 'check_to_lure_into_double_capture')
+
+  solve_n(1, rules, 'checks_moves')
+  return
+
+  let link = puzzles[0].link
+  console.log(link)
+  let fen = puzzles[0].move_fens[0]
+
+  //fen = '8/3Qnk1p/8/4B2b/Pp2p3/1P2P3/5PPP/2rR2K1 b - - 6 33'
+  solve_n(0, rules, 'checks_moves')
+})
+
