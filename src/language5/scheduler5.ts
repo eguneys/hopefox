81c872c22b5112120369f81a3bad9e925ac6e7a1
eguneys <eguneys@gmail.com>
2026-01-18 07:40:53 +0300

    language dsl relation manager

--- src/language5/scheduler5.ts
+++ src/language5/scheduler5.ts
@@ -1,5 +1,6 @@
 import { ColorC, PieceTypeC } from "../distill/hopefox_c"
 import { Square } from "../distill/types"
+import { NodeId } from "../language1/node_manager"
 import { BaseRow, RelationManager } from "./relation_manager"
 
 
@@ -74,24 +75,100 @@ export class Rs {
     evades: StatefulRelationManager<Evades>
 
     constructor() {
-        this.legals = new StatefulRelationManager()
-        this.attacks = new StatefulRelationManager()
-        this.attacks2 = new StatefulRelationManager()
-        this.occupies = new StatefulRelationManager()
-        this.captures = new StatefulRelationManager()
-        this.checks = new StatefulRelationManager()
-        this.forks = new StatefulRelationManager()
-        this.evades = new StatefulRelationManager()
+        this.legals = new StatefulRelationManager(this, materialize_legals)
+        this.attacks = new StatefulRelationManager(this, materialize_attacks)
+        this.attacks2 = new StatefulRelationManager(this, materialize_attacks2)
+        this.occupies = new StatefulRelationManager(this, materialize_occupies)
+        this.captures = new StatefulRelationManager(this, materialize_captures)
+        this.checks = new StatefulRelationManager(this, materialize_checks)
+        this.forks = new StatefulRelationManager(this, materialize_forks)
+        this.evades = new StatefulRelationManager(this, materialize_evades)
+    }
+
+    step() {
+        this.legals.step()
+        this.attacks.step()
+        this.attacks2.step()
+        this.occupies.step()
+        this.captures.step()
+        this.checks.step()
+        this.forks.step()
+        this.evades.step()
     }
 }
+
+type MaterializeFn = <T extends Row>(r: StatefulRelationManager<T>) => boolean
+type WorldId = NodeId
 
 export class StatefulRelationManager<T extends Row> {
     public state: MaterializeState
     public relation: RelationManager<T>
 
-    constructor() {
+    private fn: MaterializeFn
+    private Rs: Rs
+
+    private nb_used: number
+
+    constructor(Rs: Rs, fn: MaterializeFn) {
         this.relation = new RelationManager()
         this.state = MaterializeState.None
+        this.fn = fn
+        this.Rs = Rs
+
+        this.nb_used = 0
+    }
+
+    get(world_id: WorldId) {
+        if (this.state === MaterializeState.Complete) {
+            this.nb_used++
+            return this.relation.get_relation_starting_at_world_id(world_id)
+        }
+        if (this.state === MaterializeState.None) {
+            this.state = MaterializeState.Materializing
+        }
+    }
+
+    step() {
+
+        if (this.state === MaterializeState.Materializing) {
+            let complete = this.fn(this)
+            if (complete) {
+                this.state = MaterializeState.Complete
+            }
+        }
     }
 }
 
+function materialize_legals<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+
+function materialize_attacks<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+
+function materialize_attacks2<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+
+
+
+function materialize_occupies<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+function materialize_checks<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+function materialize_captures<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+function materialize_evades<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
+
+
+
+
+function materialize_forks<T extends Row>(r: StatefulRelationManager<T>): boolean {
+    throw new Error("Function not implemented.")
+}
