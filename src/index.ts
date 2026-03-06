d53ff58604eaa51d5362d1e65e5c866e569fbd4e
eguneys <eguneys@gmail.com>
2026-03-06 23:44:19 +0300

    asdfal;kjf

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ export * from './distill/fen'
 export * from './distill/san'
 export * from './distill/chess'
 export * from './distill/hopefox'
+export * from './distill/types'
+export * from './distill/squareSet'
 
 
 export * from './make_strata'
