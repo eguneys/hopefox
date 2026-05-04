19f2e62f39e9572e933919c6ef67caa7bc8f3f1f
eguneys <eguneys@gmail.com>
2026-05-04 07:50:17 +0300

    log

--- src/gof2/log.ts
+++ src/gof2/log.ts
@@ -1,6 +1,22 @@
-import { Visual_CompositeNestedGraphNode, Visual_CompositeNestedGraphRoot } from "./types";
+import { Visual_CompositeActionCall, Visual_CompositeNestedGraphNode, Visual_CompositeNestedGraphRoot } from "./types";
 
 export function visual_node_log(v: Visual_CompositeNestedGraphRoot) {
+    let res = ''
 
+    const get_witness = (call: Visual_CompositeActionCall) => call.witness.slice(0, 1).map(_ => `{ ${_.join(' ')} }`).join(' ') + (call.witness.length > 1 ? '..' : '')
 
+    function print_node(v: Visual_CompositeNestedGraphNode, indent: number) {
+        let aa = ' '.repeat(indent)
+
+        for (let call of v.data.call) {
+            let call_name = call.name.match(/(.*)\((.*)\)/)![1]
+            res += `${aa}${call_name} ${get_witness(call)}\n`
+        }
+
+        v.children.map(_ => print_node(_, indent + 2))
+    }
+
+    v.map(_ => print_node(_, 0)).join('\n\n')
+
+    return res
 }
